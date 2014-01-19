fa35b1e41ba63105f0a3096d30c4d96922252db3
詹昊恂 <programmer.zhx@gmail.com>
2014-01-19 17:46:45 +0800

    adjust the number of <data>

--- interface.py
+++ interface.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
-Usage: mgd [-f <from_lang>] [-t <to_lang>] [-v|--reverse] <data>
-       mgd -r|--record
+Usage: mgd [--debug] [-f <from_lang>] [-t <to_lang>] [-v|--reverse] <data>...
+       mgd [--debug] -r|--record
        mgd -h|--help
 
 Options:
@@ -9,20 +9,34 @@ Options:
     -t <to_lang>    ouput language [default: zh-CN]
     -v --reverse    reverse -f and -t
     -r --record     display search record
+    --debug         display runtime information
 """
 from docopt import docopt
 from translate import Translator
 from record import Record
 
+
+def _assemble_data(raw_data):
+    # raw_data should be a list, according to docopt's document.
+    assert type(raw_data) is list
+
+    if len(raw_data) == 1:
+        # single word
+        return raw_data[0]
+    elif len(raw_data) > 1:
+        # multi-word sentence.
+        return ' '.join(raw_data)
+
+
 if __name__ == '__main__':
-    arguements = docopt(__doc__)
+    arguements = docopt(__doc__, version='0.1')
     record = Record()
     if arguements['<data>']:
         # translation
         from_lang = arguements['-f']
         to_lang = arguements['-t']
         reverse = arguements['--reverse']
-        data = arguements['<data>']
+        data = _assemble_data(arguements['<data>'])
         # reverse langs
         from_lang, to_lang = (to_lang, from_lang)\
             if reverse else (from_lang, to_lang)
@@ -32,11 +46,12 @@ if __name__ == '__main__':
         # result is a dictionary contains decoded infomation of the
         # trnaslation.
         result = translator.translate()
-
         translator.display_result(result)
         # add record
         record.add(from_lang, to_lang, data, result)
 
     elif arguements['--record']:
-        # display
+        # display record
         record.display()
+    else:
+        raise Exception('No Implemented Yet.')
