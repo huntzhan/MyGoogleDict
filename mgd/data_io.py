46e0793d2aad56dfa1f69e56479a2e1ba8530f43
詹昊恂 <programmer.zhx@gmail.com>
2014-02-23 21:22:58 +0800

    change read/write mode of records to bytes.

--- mgd/data_io.py
+++ mgd/data_io.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 from __future__ import print_function
 
 import os
@@ -9,22 +10,23 @@ except:
     import configparser
 
 
-# _DATA_DIR hard coded the directory contains records and configuation file.
-_DATA_DIR = os.path.expanduser('~/.mgd')
+class _GLOBAL:
 
-_CONFIG_FILENAME = 'config'
-_CACHE_FILENAME = 'cache.xml'
-_RECORD_FILENAME = 'record.xml.gz'
+    # _DATA_DIR hard coded the directory contains records and configuation
+    # file.
+    DATA_DIR = os.path.expanduser('~/.mgd')
 
+    CONFIG_FILENAME = 'config'
+    CACHE_FILENAME = 'cache.xml'
+    RECORD_FILENAME = 'record.xml.gz'
 
-# fields in configuration file.
-_SECTION_NAME = 'MyGoogleDict'
-_FROM_LANG = 'default_from_lang'
-_TO_LANG = 'default_to_lang'
-_AUDIO_PLAYBACK_COMMAND = 'audio_playback_command'
+    # fields in configuration file.
+    SECTION_NAME = 'MyGoogleDict'
+    FROM_LANG = 'default_from_lang'
+    TO_LANG = 'default_to_lang'
+    AUDIO_PLAYBACK_COMMAND = 'audio_playback_command'
 
-
-_RAW_CONTENT = """
+    RAW_CONTENT = """
 [{}]
 {}: en
 {}: zh-CN
@@ -36,36 +38,53 @@ def _generate_default_config_content():
     import platform
     audio_player = 'afplay' if platform.system() == 'Darwin' else 'mpg123'
 
-    content = _RAW_CONTENT.format(
-        _SECTION_NAME,
-        _FROM_LANG,
-        _TO_LANG,
-        _AUDIO_PLAYBACK_COMMAND, audio_player,
+    content = _GLOBAL.RAW_CONTENT.format(
+        _GLOBAL.SECTION_NAME,
+        _GLOBAL.FROM_LANG,
+        _GLOBAL.TO_LANG,
+        _GLOBAL.AUDIO_PLAYBACK_COMMAND, audio_player,
     )
     return content
 
-_DEFAULT_CONFIG_CONTENT = _generate_default_config_content()
+_GLOBAL.DEFAULT_CONFIG_CONTENT = _generate_default_config_content()
 
 
 class RecordIO(object):
 
     def __init__(self):
         self._cache_path = os.path.join(
-            _DATA_DIR,
-            _CACHE_FILENAME,
+            _GLOBAL.DATA_DIR,
+            _GLOBAL.CACHE_FILENAME,
         )
         self._record_path = os.path.join(
-            _DATA_DIR,
-            _RECORD_FILENAME,
+            _GLOBAL.DATA_DIR,
+            _GLOBAL.RECORD_FILENAME,
         )
 
     def _read_file(self, path, gzip_enable=False):
+        """
+        Parameters:
+            path: points to file to be read.
+            gzip_enable: Ture for using gzip.open(), False for using built-in
+                         open().
+        Return:
+            Content of file represented in bytes.
+        """
+
         openfile = gzip.open if gzip_enable else open
-        with openfile(path) as f:
+        with openfile(path, modw='rb') as f:
             content = f.read()
         return content
 
     def _write_file(self, content, path, gzip_enable=False):
+        """
+        Parameters:
+            content: bytes of content to be written to file.
+            others: equivalent to _read_file.
+        Return:
+            None.
+        """
+
         openfile = gzip.open if gzip_enable else open
         with openfile(path, 'wb') as f:
             f.write(content)
@@ -98,13 +117,18 @@ class RecordIO(object):
     # Merge Flag #
     ##############
     def _judge_merge(self):
+        """
+        Return:
+            True for merging file.
+        """
+
         # hardcode the threshold size of cache .
         MAX_SIZE = 65535
         try:
             file_size = os.path.getsize(self._cache_path)
         except:
             # file not exist, force to merge.
-            file_size = MAX_SIZE + 1
+            return True
 
         if file_size > MAX_SIZE:
             return True
@@ -122,39 +146,46 @@ class ConfigIO(object):
 
     def _init_config(self):
         # check existence of data dir.
-        if not os.path.exists(_DATA_DIR):
-            os.makedirs(_DATA_DIR)
-        # check existence of configuration file
-        path = os.path.join(_DATA_DIR, _CONFIG_FILENAME)
+        if not os.path.exists(_GLOBAL.DATA_DIR):
+            os.makedirs(_GLOBAL.DATA_DIR)
+
+        path = os.path.join(
+            _GLOBAL.DATA_DIR,
+            _GLOBAL.CONFIG_FILENAME,
+        )
+        # assure existence of configuration file
         if not os.path.exists(path):
-            # generate default configuation file
+            # generate default configuation file.
+            # text mode is required, for writing unicode literals.
             with open(path, 'w') as f:
-                f.write(_DEFAULT_CONFIG_CONTENT)
+                f.write(_GLOBAL.DEFAULT_CONFIG_CONTENT)
+        # finally, return that path.
         return path
 
     def _open_config(self):
         path = self._init_config()
-        # return file
+        # return file object, with default mode wt.
         return open(path)
 
     def set_up_doc(self, doc):
         return doc.format(
-            default_from_lang=self._config.get(_SECTION_NAME, _FROM_LANG),
-            default_to_lang=self._config.get(_SECTION_NAME, _TO_LANG),
+            default_from_lang=self._config.get(_GLOBAL.SECTION_NAME,
+                                               _GLOBAL.FROM_LANG),
+            default_to_lang=self._config.get(_GLOBAL.SECTION_NAME,
+                                             _GLOBAL.TO_LANG),
         )
 
     def get_playback_command(self):
-        return self._config.get(_SECTION_NAME, _AUDIO_PLAYBACK_COMMAND)
-
-
-# mantain a global configuration handler
-_config_io = ConfigIO()
+        return self._config.get(_GLOBAL.SECTION_NAME,
+                                _GLOBAL.AUDIO_PLAYBACK_COMMAND)
 
 
 def set_up_doc(doc):
-    return _config_io.set_up_doc(doc)
+    config_io = ConfigIO()
+    return config_io.set_up_doc(doc)
 
 
 def get_playback_command():
-    raw_command = _config_io.get_playback_command()
+    config_io = ConfigIO()
+    raw_command = config_io.get_playback_command()
     return raw_command.split(' ')
